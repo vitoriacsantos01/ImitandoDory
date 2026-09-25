549cd38f58c0b3d9045882482b79580eab5cd89f
vitoria <vitoriacarneirodossantos853@gmail.com>
2026-09-25 03:20:29 -0300

    falando baleiês!!

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine();
+
+string frase;
+string traduzido;
+
+Console.Write("digite uma frase e fale baleiês ;) : ");
+frase = Console.ReadLine()!;
+
+Console.WriteLine();
+
+traduzido = frase
+.Replace("a", "aaa")
+.Replace("A", "AAA")
+.Replace("e", "eee")
+.Replace("E", "EEE")
+.Replace("i", "iii")
+.Replace("I", "III")
+.Replace("o", "ooo")
+.Replace("O", "OOO")
+.Replace("u", "uuu")
+.Replace("U", "UUU");
+            
+Console.WriteLine($"Em baleiês: {traduzido}");
+
+Console.WriteLine();
